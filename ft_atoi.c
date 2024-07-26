9ee425e894389b5fab83aeec85853a4848f7971e
Iryna Stasheuski <istasheu@c1r1p6.hive.fi>
2024-07-26 15:45:34 +0300

    ref: ft_atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: istasheu <istasheu@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 08:41:52 by istasheu          #+#    #+#             */
-/*   Updated: 2023/11/21 10:41:17 by istasheu         ###   ########.fr       */
+/*   Updated: 2024/07/26 15:44:31 by istasheu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,27 @@
 
 int	ft_atoi(const char *str)
 {
-	long int	nb;
-	int			isneg;
-	int			i;
+	int				is_negative;
+	long long		number;
 
-	nb = 0;
-	isneg = 1;
-	i = 0;
-	if (str[i] == '-' || str[i] == '+')
+	is_negative = 1;
+	number = 0;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	if (*str == '-' || *str == '+')
 	{
 		if (*(str++) == '-')
-			isneg = -1;
+			is_negative = -1;
 	}
-	while (ft_isdigit(str[i]))
+	while (*str >= 48 && *str <= 57)
 	{
-		nb = (nb * 10) + (str[i] - '0');
-		i++;
+		number = number * 10 + (*(str++) - '0');
+		if (number < 0)
+		{
+			if (is_negative > 0)
+				return (-1);
+			return (0);
+		}
 	}
-	nb *= isneg;
-	if (!(INT_MIN <= nb && nb <= INT_MAX))
-	{
-		ft_putstr_fd("Error\n", 2);
-		exit (1);
-	}
-	return (nb);
+	return ((int)number * is_negative);
 }
